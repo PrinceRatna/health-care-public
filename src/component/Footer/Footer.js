ef1c76fbf85134eb90668bb03970d6efc45457e1
PrinceRatna <arjnry125@gmail.com>
2021-10-20 13:09:23 +0600

    first commit10

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
              <h1 className="font-semibold">CONTACT US</h1>
              <hr/>
              <span className="inline-block mr-5 mt-5">{element2}  princeratna125@gmail.com</span><br/>
-            <span className="inline-block mr-5 mt-5">{element3} AZimpur,1243 Clony</span><br/>
+            <span className="inline-block mr-5 mt-5">{element3} Mirpur ,1243 Clony</span><br/>
              <span className="inline-block mr-5 mt-5">{element1} 017712155</span><br/>
             <span className="inline-block mr-5 mt-5">{element4 }   </span>
             <span className="inline-block mr-5 mt-5">{element5}</span>
@@ -24,20 +24,21 @@ const Footer = () => {
          <div>
              <h1 className="font-semibold" >USE FULL LINKS</h1>
              <hr/>
-             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=T5Spgwegpbo">https://www.youtube.com/watch?</a> <br/>
-             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=T5Spgwegpbo">https://www.youtube.jouu</a><br/>
-             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=_4qQQMKvkMI">https://www.youtube.com</a><br/>
-             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=_4qQQMKvkMI">https://www.youtube</a><br/>
-             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=_4qQQMKvkMI">https://www.youtube.4qQQMKvkMI</a>
+             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=kaO6XRaTieM">https://www.youtube.com/watch?</a> <br/>
+             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=6qFCNJD4vk0">https://www.youtube.jouu</a><br/>
+             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=jsl8Gx9CUfU">https://www.youtube.com</a><br/>
+             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=hUHCadgWZO8">https://www.youtube</a><br/>
+             <a className="mt-5 inline-block" href="https://www.youtube.com/watch?v=mhmcTP_rz2M">https://www.youtube.4qQQMKvkMI</a>
          </div>
          <div>
-             <h1 className="font-semibold">LATEST COURSES</h1>
+             <h1 className="font-semibold">LATEST SERVICES</h1>
              <hr/>
              <ul>
-                 <li>Physics</li>
-                 <li>Chemistry</li>
-                 <li>Mathematics</li>
-                 <li>Biology</li>
+                 <li>Kidney operation</li>
+                 <li>Eye operation
+                 </li>
+                 <li>Brain operation</li>
+                 <li>Corona unit</li>
              </ul>
             </div>
         </footer>
