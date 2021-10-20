2d2c1266e5f6e4317fde507f4c07a026b4dfec13
PrinceRatna <arjnry125@gmail.com>
2021-10-20 12:52:58 +0600

    first commit9

--- src/component/Detail/Detail.js
+++ src/component/Detail/Detail.js
@@ -4,22 +4,30 @@ import { useParams } from 'react-router';
 const Detail = () => {
     // console.log(props.detail);
   let {detailId}=useParams();
-  const [service,setService]=useState([]);
+  const [service,setService]=useState({});
 
   useEffect(()=>{
-    fetch(`http://localhost:3000/serviceId/${detailId}.json`)
-    // fetch(`https://healthcare-6f719/serviceId/${detailId}`)
-        .then(res=>res.json())
-    .then(data=>setService(data));
 
-  },[detailId])
+    fetch ('../services1.json')
+    .then(res=>res.json())
+    .then(data=>setService(data[detailId-1]))
   
-    return (
-        <div className="mt-10 mb-20 text-center mx-10">
-        
-            <p>{service[0]?.detail}</p>
-        </div>
-    );
-};
+
+  },[])
+
+ 
+  return(
+<div>
+  
+    {
+ 
+ service.detail&&<div className="mt-10 mb-20 text-center mx-10">{service.detail}</div>
+  
+    }
+  
+  
+</div>
+  );
+}
 
 export default Detail;
--- src/component/Service/Service.js
+++ src/component/Service/Service.js
@@ -9,7 +9,9 @@ const Service = (props) => {
         <div className=" mx-auto rounded">
             <img className="w-96 h-72" src={url} alt=''/>
             <h1 className="text-lg md:text-2xl font-semibold">{title}</h1>
-            <h1 className="text-lg md:text-2xl font-semibold">Price: ${price}</h1>
+            <h1 className="text-lg md:text-xl font-semibold">Detail : <span className="text-sm md:text-base font-medium"> {detail.slice(0,100)}</span> </h1>
+
+            <h1 className="text-lg md:text-xl font-medium">Price: ${price}</h1>
 <NavLink className=" inline-block bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 w-16 h-6 text-white font-medium text-base rounded" to={`/detail/${id}`}> Detail</NavLink>
         </div>
     );
