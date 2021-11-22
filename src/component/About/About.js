50a364b53484e1e6b977a746e1af1967bb445a90
PrinceRatna <arjnry125@gmail.com>
2021-11-22 23:58:23 +0600

    first commit

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -12,6 +12,8 @@ const About = () => {
    Opening its doors in August 2006 and situated beside the picturesque Gulshan Lake, this hospital is    one of the largest private sector healthcare facilities in Bangladesh. United Hospital has the    capacity to house over 500 patients and has 12 state of the art operation theatres to cater to the    needs of varied patient base.With more than 30 clinical sub specialties, our centres of Excellence    like Cardiac, Cancer, Renal, Surgical, Neuroscience, Mother & Child Health and Critical Care Centres    are staffed by the most esteemed doctors in their respective fields. Since inception till date (July    2021) we have conducted 420,000 Cardiac Investigations, 55,000 Cardiac Procedures, 14,000 Heart    Surgeries with 98% success rate, 39 Kidney Transplant, 295,000 Haemodialysis, 185,000 Radiation    Therapy Sessions & 196,000 Health Checkups completed.
    </p>
             </div>
+
+            
            
         </div>
     );
